6527bb96634e3b7863f4322456fdaf2c42b802a0
Franck Charras <franck.charras@inria.fr>
2022-08-08 15:57:46 +0200

    Working POC

--- sklearn_numba_dpex/examples/kmeans_numba_dpex.py
+++ sklearn_numba_dpex/examples/kmeans_numba_dpex.py
@@ -285,6 +285,7 @@ def get_initialize_to_zeros_kernel_2(thread_group_size, n, dim):
 
     return initialize_to_zeros[n_threads, thread_group_size]
 
+
 def get_update_means_step_fine_kernel(thread_group_size, n, dim):
 
     nb_items = n * dim
@@ -303,94 +304,74 @@ def get_update_means_step_fine_kernel(thread_group_size, n, dim):
         dpex.atomic.add(centroids, (centroid, col), x[row, col])
 
         if col == 0:
-            dpex.atomic.add(centroid_counts, row, 1)
+            dpex.atomic.add(centroid_counts, centroid, 1)
 
     return update_means_step_fine[n_threads, thread_group_size]
+
 
 def get_update_means_step_shm_kernel(p, thread_group_size, n, dim, n_clusters):
     nb_items = n * dim
     item_per_group = thread_group_size * p
     n_threads = math.ceil(nb_items / item_per_group) * thread_group_size
-    max_nb_points_per_group = (item_per_group // dim) + 1
-    local_centroid_shm_shape = (max_nb_points_per_group, dim)
+    max_nb_points_per_thread = (p // dim) + 1
+    max_local_centroids_nb = min((item_per_group // dim) + 1, n_clusters)
+    local_centroid_shm_shape = (max_local_centroids_nb, dim)
 
-    # ???: this should minimize divergence ?
-    # but unreasonable for dimension more than p ?
-    # if p > dim:
-    #     p = math.floor(p / dim) * dim
+    n_clusters_per_thread = math.ceil(n_clusters / thread_group_size)
+    nb_centroid_entries_per_thread = math.ceil((max_local_centroids_nb * dim) / thread_group_size)
 
     @dpex.kernel
     def update_means_step_shm(x, assignments_idx, centroid_counts, centroids):
-        '''
-        NB: here we do not implement exactly what is given in the paper
-        At this point we don't want to mindlessly trigger the caching of "centroids"
-        in each work group, because we might not have enough shared memory for it.
-        In fact, the paper implements a finer mechanism: it caches the maximum
-        amount of centroids, and for those that can't fit in, caching is ignored
-        and results are directly written to global memory. This requires at least
-        having the information of how much shared memory is available for the current
-        gpu. It seems possible (since clinfo gives it, maybe see pyopencl ?) but is
-        left for a future TODO.
-
-        What we do instead to limit the amount of shared memory is to locally remap
-        the indexes of the centroids that are accessed by the current workgroup, so
-        that only the relevant centroids are loaded in cache, rather than the whole
-        array of centroids.
-
-        Shared memory footprint is decreased at the cost of storing two additional
-        arrays of size (n_clusters) per work group, and a few computations for the
-        remapping.
-        '''
         group = dpex.get_group_id(0)
         thread = dpex.get_local_id(0)
 
-        group_nb_previous_entries = group * item_per_group
-        thread_nb_previous_entries = group_nb_previous_entries + thread * p
-
-        thread_starting_pt = (thread_nb_previous_entries // dim)
-        thread_end_pt = ((thread_nb_previous_entries + p - 1) // dim)
-
-        thread_starting_dim = thread_nb_previous_entries % dim
-
-        thread_alloc_start = thread_starting_pt if ((thread == 0) or thread_starting_dim == 0) else (thread_starting_pt + 1)
-
         group_nb_clusters = dpex.local.array(shape=1, dtype=int32)
-        local_cluster_to_global = dpex.local.array(shape=max_nb_points_per_group, dtype=uint32)
+        local_cluster_to_global = dpex.local.array(shape=max_local_centroids_nb, dtype=uint32)
+        partial_cluster_count = dpex.local.array(shape=max_local_centroids_nb, dtype=int32)
         global_cluster_to_local = dpex.local.array(shape=n_clusters, dtype=uint32)
-        local_cluster_count = dpex.local.array(shape=max_nb_points_per_group, dtype=int32)
-        local_assignments_idx = dpex.local.array(shape=max_nb_points_per_group, dtype=uint32)
-        partial_cluster_count = dpex.local.array(shape=max_nb_points_per_group, dtype=int32)
+        local_cluster_count = dpex.local.array(shape=n_clusters, dtype=int32)
         local_centroids = dpex.local.array(shape=local_centroid_shm_shape, dtype=float32)
 
-        # TODO: merge partial cluster count and local cluster count
-        for i in range(thread_alloc_start, thread_end_pt + 1):
-            cluster_idx = assignments_idx[i]
+        group_nb_previous_entries = group * item_per_group
+        thread_nb_previous_entries = group_nb_previous_entries + (thread * p)
+        thread_starting_pt = (thread_nb_previous_entries // dim)
+        thread_starting_dim = thread_nb_previous_entries % dim
+
+        if thread == 0:
+            group_nb_clusters[0] = int32(0)
+
+        starting_local_centroid = n_clusters_per_thread * thread
+        for i in range(n_clusters_per_thread):
+            idx = starting_local_centroid + i
+            if idx >= n_clusters:
+                break
+            local_cluster_count[idx] = int32(0)
+
+        dpex.barrier()
+
+        for i in range(max_nb_points_per_thread):
+            global_idx = thread_starting_pt + i
+            if global_idx >= n:
+                break
+            cluster_idx = assignments_idx[global_idx]
             cluster_count = dpex.atomic.add(local_cluster_count, cluster_idx, 1)
             if cluster_count == 0:
                 local_cluster_idx = dpex.atomic.add(group_nb_clusters, 0, 1)
                 local_cluster_to_global[local_cluster_idx] = cluster_idx
                 global_cluster_to_local[cluster_idx] = local_cluster_idx
-            local_assignments_idx[i] = global_cluster_to_local[cluster_idx]
 
         dpex.barrier()
 
-        group_nb_clusters_ = group_nb_clusters[0]
-
-        # TODO; replace with this line
-        # nb_centroid_entries_per_thread = math.ceil((group_nb_clusters_ * dim) / thread_group_size)
-        # if math.ceil is fixed to return int instead of float64
-        local_nb_centroid_items = group_nb_clusters_ * dim
-        nb_centroid_entries_per_thread = local_nb_centroid_items // thread_group_size
-        if (local_nb_centroid_items % thread_group_size) > 0:
-                nb_centroid_entries_per_thread += 1
-
         starting_entry = nb_centroid_entries_per_thread * thread
 
-        for i in range(starting_entry, starting_entry + nb_centroid_entries_per_thread):
-            row = i // dim
-            if row > group_nb_clusters_:
+        for i in range(nb_centroid_entries_per_thread):
+            idx = i + starting_entry
+            row = idx // dim
+            if row >= max_local_centroids_nb:
                 break
-            col = i % dim
+            col = idx % dim
+            if col == 0:
+                partial_cluster_count[row] = int32(0)
             local_centroids[row, col] = float32(0.)
 
         dpex.barrier()
@@ -401,37 +382,41 @@ def get_update_means_step_shm_kernel(p, thread_group_size, n, dim, n_clusters):
         # a multiple of dim (and always ensure that p >= dim)
         current_pt = thread_starting_pt
         current_dim = thread_starting_dim
-        local_cluster_idx = local_assignments_idx[current_pt]
+        current_local_pt = 0
+        local_cluster_idx = int32(global_cluster_to_local[assignments_idx[current_pt]])
         for pi in range(p):
             if current_pt >= n:
                 break
+
             dpex.atomic.add(
                 local_centroids, (local_cluster_idx, current_dim),
                 x[current_pt, current_dim])
+
             if current_dim == 0:
                 dpex.atomic.add(partial_cluster_count, local_cluster_idx, 1)
+
             current_dim = (current_dim + 1) % dim
             if current_dim == 0:
                 current_pt += 1
-                local_cluster_idx = local_assignments_idx[current_pt]
+                current_local_pt += 1
+                local_cluster_idx = int32(global_cluster_to_local[assignments_idx[current_pt]])
 
         dpex.barrier()
 
-        # current_row = starting_entry // dim
-        # local_cluster_idx = local_assignments_idx[row]
-        # global_cluster_idx = local_cluster_to_global[local_cluster_idx]
-        # for i in range(starting_entry, starting_entry + nb_centroid_entries_per_thread):
-        #     row = i // dim
-        #     if row > group_nb_clusters_:
-        #         break
-        #     if row != current_row:
-        #         local_cluster_idx = local_assignments_idx[row]
-        #         global_cluster_idx = local_cluster_to_global[local_cluster_idx]
-        #         current_row = row
-        #     col = i % dim
-        #     if col == 0:
-        #         dpex.atomic.add(centroid_counts, global_cluster_idx, partial_cluster_count[local_cluster_idx])
-        #     dpex.atomic.add(centroids, (global_cluster_idx, dim), local_centroids[row, col])
+        group_nb_clusters_ = group_nb_clusters[0]
+        current_row = -1
+        for i in range(nb_centroid_entries_per_thread):
+            idx = i + starting_entry
+            row = idx // dim
+            if row >= group_nb_clusters_:
+                break
+            if row != current_row:
+                global_cluster_idx = int32(local_cluster_to_global[row])
+                current_row = row
+            col = idx % dim
+            if col == 0:
+                dpex.atomic.add(centroid_counts, global_cluster_idx, partial_cluster_count[row])
+            dpex.atomic.add(centroids, (global_cluster_idx, int(col)), local_centroids[row, col])
 
     return update_means_step_shm[n_threads, thread_group_size]
 
@@ -470,7 +455,7 @@ def kmeans_fit_numba_dpex(x_train,
     # suggested in the paper, but it seems that the conditions are slightly
     # different when reading the implementation of the authors. Find the
     # differences between the two and use the appropriate best conditions.
-    if n_clusters <= 32 or dim >= 64 or True:
+    if n_clusters <= 32 or dim >= 64:
         h = 16
         r = 32
         thread_group_size = 256
@@ -492,11 +477,9 @@ def kmeans_fit_numba_dpex(x_train,
         thread_group_size = 256
         update_means_step = get_update_means_step_fine_kernel(thread_group_size, n, dim)
     else:
-        p = 1
+        p = 128
         thread_group_size = 256
-        # TODO: NOT READY YET
-        # update_means_step = get_update_means_step_shm_kernel(p, thread_group_size, n, dim, n_clusters)
-        update_means_step = get_update_means_step_fine_kernel(thread_group_size, n, dim)
+        update_means_step = get_update_means_step_shm_kernel(p, thread_group_size, n, dim, n_clusters)
 
     initialize_to_zeros_1 = get_initialize_to_zeros_kernel_1(thread_group_size=256, n=n_clusters)
     initialize_to_zeros_2 = get_initialize_to_zeros_kernel_2(thread_group_size=256, n=n_clusters, dim=dim)
@@ -508,8 +491,6 @@ def kmeans_fit_numba_dpex(x_train,
     if n_cluster_groups > 1:
         fine_assignments_dist = dpctl.tensor.empty(n, dtype=np.float32)
         fine_assignments_idx = dpctl.tensor.empty(n, dtype=np.uint32)
-
-    if n_cluster_groups > 1:
         fine_assignment_step(
                 coarse_assignments_dist, coarse_assignments_idx,
                 fine_assignments_dist, fine_assignments_idx)
@@ -519,6 +500,7 @@ def kmeans_fit_numba_dpex(x_train,
 
     initialize_to_zeros_2(centroids)
     initialize_to_zeros_1(centroid_counts)
+
     update_means_step(x_train, fine_assignments_idx, centroid_counts, centroids)
     print("Finished one iteration.")
 
