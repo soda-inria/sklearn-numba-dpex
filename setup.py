3cfbc79bb484d398403dc8bd596c1cb523a2f593
Franck Charras <franck.charras@inria.fr>
2022-08-26 16:56:53 +0200

    Move some informations from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,45 +1,6 @@
 from setuptools import setup
 
 
-LONG_DESCRIPTION = """\
-TODO
-"""
-
-
 setup(
-    name="sklearn-numba-dpex",
-    maintainer="Olivier Grisel",
-    maintainer_email="olivier.grisel@ensta.org",
-    description="Computational Engine for scikit-learn based on numba-dpex",
-    license="BSD 3-Clause License",
-    url="https://github.com/ogrisel/sklearn-numba-dpex",
-    version="0.1.0",
-    long_description=LONG_DESCRIPTION,
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved",
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Scientific/Engineering",
-        "Development Status :: 4 - Beta",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
-        "Operating System :: MacOS",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: Implementation :: CPython",
-    ],
-    python_requires=">=3.8",
-    # TODO: remove pyopencl when dpctl exposes preferred_work_group_size_multiple
-    # see https://github.com/IntelPython/dpctl/issues/886
-    install_requires=["scikit-learn", "numba-dpex", "dpctl", "pyopencl"],
-    extras_require=dict(benchmark=["scikit-learn-intelex", "pandas"]),
     packages=["sklearn_numba_dpex"],
-    entry_points={
-        "sklearn_engines": ["kmeans=sklearn_numba_dpex.cluster.kmeans:KMeansEngine"]
-    },
 )
